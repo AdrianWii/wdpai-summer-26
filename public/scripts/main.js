f0b1c4de26c6ffd5d0184940cc05792732665a4a
Adrian Widłak <adrian.widlak@interia.pl>
2026-03-29 10:27:37 +0200

    Hamburger menu

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,10 @@
-console.log("main js")
+const menuIcon = document.querySelector(".display-mobile.fa-bars");
+const navList = document.querySelector("nav > div.container > ul");
+
+menuIcon.addEventListener("click", () => {
+  if (navList.style.display === "block") {
+    navList.style.display = "none";
+  } else {
+    navList.style.display = "block";
+  }
+});
