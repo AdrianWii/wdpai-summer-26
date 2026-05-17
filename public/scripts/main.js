845ed3aecca66189ad1348c94f1bc63ae7633697
Adrian Widłak <adrian.widlak@interia.pl>
2026-05-17 10:19:21 +0200

    Basic JS validation

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,9 +2,9 @@ const menuIcon = document.querySelector(".display-mobile.fa-bars");
 const navList = document.querySelector("nav > div.container > ul");
 
 menuIcon.addEventListener("click", () => {
-  if (navList.style.display === "block") {
+  if (navList.style.display === "flex") {
     navList.style.display = "none";
   } else {
-    navList.style.display = "block";
+    navList.style.display = "flex";
   }
 });
